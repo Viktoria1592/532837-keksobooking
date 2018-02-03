8ce89c28fbad2e0dbc7b372ae3e1ffd37f321f84
Andrevwv <andrevwv@gmail.com>
2018-02-03 18:10:47 +0200

    added comments with functions description

--- js/map.js
+++ js/map.js
@@ -8,18 +8,35 @@ var checkinTimes = ['12:00', '13:00', '14:00'];
 var checkoutTimes = ['12:00', '13:00', '14:00'];
 var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var images = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var FLAT_IMAGES_HEIGHT = 90;
 
-
+/**
+ * Функция генерирующая случайное значение
+ * в диапазоне от min до max
+ * @param  {number} min Минимальное значение
+ * @param  {number} max Максимальное значение
+ * @return {number}
+ */
 var generateRandomNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 };
 
+/**
+ * Функция генерирования случайного значения из переданного массива
+ * @param  {array} array
+ * @return {number}     случайное значение из массива
+ */
 var generateRandomArrayValue = function (array) {
   var randomNumber = generateRandomNumber(0, array.length);
-  var randomArrayValue = array[randomNumber];
-  return randomArrayValue;
+  return array[randomNumber];
 };
 
+/**
+ * Функция генерирования случайного значения из переданного массива
+ * (из переданного в функцию массива удаляется возвращаемое значение)
+ * @param  {array} array
+ * @return {number}      случайное значение из массива
+ */
 var generateRandomNonRepeatableArrayValue = function (array) {
   var randomNumber = generateRandomNumber(0, array.length);
   var randomArrayValue = array[randomNumber];
@@ -27,6 +44,11 @@ var generateRandomNonRepeatableArrayValue = function (array) {
   return randomArrayValue;
 };
 
+/**
+ * Функция перемешивания данных внутри массива
+ * @param  {array} array
+ * @return {array}      перемешанный массив
+ */
 var shuffleArray = function (array) {
   for (var i = 0; i < array.length; i++) {
     var randomNumber = generateRandomNumber(0, array.length);
@@ -37,6 +59,12 @@ var shuffleArray = function (array) {
   return array;
 };
 
+/**
+ * Функция генерации массива случайной длинны,
+ * не длиннее чем массив, который в нее передается
+ * @param  {array} array массив из которого генерируется случайный
+ * @return {array}       массив случайной длинны (не длиннее чем переданный)
+ */
 var generateArrayWithRandomLenght = function (array) {
   var newArray = [];
   var numberOfArrayItems = generateRandomNumber(1, array.length + 1);
@@ -52,15 +80,25 @@ for (var l = 0; l < AVATAR_COUNT; l++) {
   avatarImages[l] = l + 1;
 }
 
-var generateAdvertObject = function () {
+/**
+ * Функция генерации объекта с данными объявления
+ * @param  {number} step номер изображения автора
+ * @return {object}      объект с данными объявления
+ */
+var generateAdvertObject = function (step) {
+  var location = {
+    'x': generateRandomNumber(300, 900),
+    'y': generateRandomNumber(150, 500)
+  };
+
   var newAdvertObject = {
     'author': {
-      'avatar': 'img/avatars/user0' + generateRandomNonRepeatableArrayValue(avatarImages) + '.png'
+      'avatar': 'img/avatars/user0' + (step + 1) + '.png'
     },
 
     'offer': {
       'title': generateRandomNonRepeatableArrayValue(headers),
-      'address': '{{location.x}}, {{location.y}}',
+      'address': location.x + ', ' + location.y,
       'price': generateRandomNumber(1000, 1000000),
       'type': generateRandomArrayValue(typesOfFlat),
       'rooms': generateRandomNumber(1, 5),
@@ -73,8 +111,8 @@ var generateAdvertObject = function () {
     },
 
     'location': {
-      'x': generateRandomNumber(300, 900),
-      'y': generateRandomNumber(150, 500)
+      'x': location.x,
+      'y': location.y
     }
   };
   return newAdvertObject;
@@ -82,7 +120,7 @@ var generateAdvertObject = function () {
 
 var adverts = [];
 for (var i = 0; i < ADVERTS_COUNT; i++) {
-  adverts[i] = generateAdvertObject();
+  adverts[i] = generateAdvertObject(i);
 }
 
 var map = document.querySelector('.map');
@@ -97,6 +135,12 @@ var renderAdvertLabel = function (advert) {
   return newAdvert;
 };
 
+/**
+ *  Функция заполняющая данными шаблон и добавляющая заполненый шаблон в DocumentFragment
+ * @param  {array}    advertsArray     массив с объектами объявлений
+ * @param  {function} renderFunction   функция заполняющая шаблон данными из advertsArray
+ * @return {Node}                      DOM с элемент объявлениями
+ */
 var fragmentFilling = function (advertsArray, renderFunction) {
   var fragment = document.createDocumentFragment();
   for (var k = 0; k < advertsArray.length; k++) {
@@ -105,23 +149,29 @@ var fragmentFilling = function (advertsArray, renderFunction) {
   return fragment;
 };
 
+var typeLabels = {
+  'flat': 'Квартира',
+  'bungalo': 'Бунгало',
+  'house': 'Дом'
+};
+
 var mapPins = document.querySelector('.map__pins');
 mapPins.appendChild(fragmentFilling(adverts, renderAdvertLabel));
 
 var cardTemplate = document.querySelector('template').content.querySelector('article.map__card');
+
+/**
+ * Функция заполняющая шаблон с деталями объявления из объекта с данными
+ * @param  {object} advert объект с данными об объявлении
+ * @return {Node}          DOM-элемент объявления
+ */
 var renderAdvertCard = function (advert) {
   var newCard = cardTemplate.cloneNode(true);
   newCard.querySelector('h3').textContent = advert.offer.title;
   newCard.querySelector('p small').textContent = advert.location.x + ', ' + advert.location.y;
   newCard.querySelector('.popup__price').textContent = advert.offer.price + ' \u20BD/ночь';
 
-  if (advert.offer.type === 'flat') {
-    newCard.querySelector('h4').textContent = 'Квартира';
-  } else if (advert.offer.type === 'house') {
-    newCard.querySelector('h4').textContent = 'Дом';
-  } else {
-    newCard.querySelector('h4').textContent = 'Бунгало';
-  }
+  newCard.querySelector('h4').textContent = typeLabels[advert.offer.type];
 
   newCard.querySelector('h4 + p').textContent = advert.offer.rooms + ' комнаты для ' + advert.offer.guests + ' гостей';
   newCard.querySelector('h4 + p + p').textContent = 'Заезд после ' + advert.offer.checkin + ', выезд до ' + advert.offer.checkout;
@@ -142,7 +192,7 @@ var renderAdvertCard = function (advert) {
   for (var n = 0; n < advert.offer.photos.length; n++) {
     var newImage = cardTemplate.querySelector('ul.popup__pictures li img').cloneNode(true);
     newImage.src = advert.offer.photos[n];
-    newImage.height = 90;
+    newImage.height = FLAT_IMAGES_HEIGHT;
     popupPictures.appendChild(newImage);
   }
 
